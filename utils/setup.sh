93609382f26097e8c2e3b7961fc8f4bb8f5f9484
Monge <kenjiyamane0@gmail.com>
2020-09-28 00:33:42 -0300

    utils

---
+++ utils/setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+sudo apt-get install git ssh vim texlive-full
+echo "set tabstop=4" >>  $HOME/.vimrc
+echo "set autoindent" >>  $HOME/.vimrc
+echo ":map z 0<Esc>" >>  $HOME/.vimrc
+echo ":map t 0i<tab><Esc>j" >>  $HOME/.vimrc
+echo ":map dt 0x" >>  $HOME/.vimrc
+echo ":map dd dd_" >>  $HOME/.vimrc
+echo ":map q \$xj" >>  $HOME/.vimrc
+echo ":map s 0i<space><space><space><space><Esc>j" >>  $HOME/.vimrc
+
+sudo apt-get install gimp poppler-utils
+
