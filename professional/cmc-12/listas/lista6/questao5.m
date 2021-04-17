36ff2c267e4b5490980048bb1482a63a30d55b01
Monge <kenjiyamane0@gmail.com>
2021-04-17 19:16:30 -0300

    cmc12 list 6

---
+++ professional/cmc-12/listas/lista6/questao5.m
@@ -0,0 +1,11 @@
+function [Kp, Ki] = questao5()
+% Considerando um circuito RC com R = 10 kOhm e C = 1 muF, calcule ganhos
+% Kp e Ki de um controlador PI com pre-filtro para que o sistema tenha
+% tempo de subidade de 0 a 100% tr = 0.01 s e sobressinal Mp = 0.046.
+
+% Kp = ...
+% Ki = ...
+    Kp = -2.8690e-7;
+    Ki = -8.1238e-5;
+
+end
