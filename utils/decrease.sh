f23c9d8954b1ae2f70228d79112d779ff16f52cd
Monk <kenjiyamane0@gmail.com>
2021-08-22 20:58:59 -0300

    add linux saved image helpers

---
+++ utils/decrease.sh
@@ -0,0 +1,3 @@
+#!/bash/bin
+
+export FILE_NUM=$(($FILE_NUM - 1))
---
+++ utils/rel-maker.sh
@@ -0,0 +1,5 @@
+#!/bash/bin
+
+export FILE_NUM=$(($FILE_NUM + 1))
+mv *.png questao${FILE_NUM}.png
+mv *.png ../Documents/College/professional/ces-30/lab2/images
