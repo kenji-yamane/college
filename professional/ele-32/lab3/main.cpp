dd5ca218557e6f1624d90952e64bf4f8de04ec6a
Monk <kenjiyamane0@gmail.com>
2022-06-10 09:49:31 -0300

    filtered graphic

--- professional/ele-32/lab3/main.cpp
+++ professional/ele-32/lab3/main.cpp
@@ -24,41 +24,48 @@ int main() {
 	probabilities.push_back(0.005);
 	std::vector<double> encoderP[3];
 	for (const auto &p : probabilities) {
-		BscCanal canal(p, r);
-
-		std::vector<Encoder> encoders{
-			Encoder(3, std::vector<int>{013, 015, 017}),
-		    Encoder(4, std::vector<int>{025, 033, 037}),
-			Encoder(6, std::vector<int>{0117, 0127, 0155})
-		};
-
-		std::vector<int> info;
-		for (int i = 0; i < 10000; i++) {
-			info.push_back(r->coinFlip());
-		}
-
-		for (auto &e : encoders) {
-			for (int i = 0; i < info.size(); i++) {
-				int output = e.encode(info[i]);
-				std::bitset<3> encoded(output);
-				int receivedOutput = 0;
-				for (int i = 0; i < 3; i++) {
-					int bitOutput = canal.transmit(encoded[i]);
-					receivedOutput += (bitOutput << i);
+		double mean[3] = {};
+		for (int i = 0; i < 20; i++) {
+			BscCanal canal(p, r);
+		
+			std::vector<Encoder> encoders{
+				Encoder(3, std::vector<int>{013, 015, 017}),
+			    Encoder(4, std::vector<int>{025, 033, 037}),
+				Encoder(6, std::vector<int>{0117, 0127, 0155})
+			};
+	
+			std::vector<int> info;
+			for (int i = 0; i < 10000; i++) {
+				info.push_back(r->coinFlip());
+			}
+	
+			for (auto &e : encoders) {
+				for (int i = 0; i < info.size(); i++) {
+					int output = e.encode(info[i]);
+					std::bitset<3> encoded(output);
+					int receivedOutput = 0;
+					for (int i = 0; i < 3; i++) {
+						int bitOutput = canal.transmit(encoded[i]);
+						receivedOutput += (bitOutput << i);
+					}
+					e.decode(receivedOutput);
 				}
-				e.decode(receivedOutput);
+			}
+	
+			for (int i = 0; i < encoders.size(); i++) {
+				std::vector<int> received = encoders[i].getSequence();
+				int errors = 0;
+				for (int j = 0; j < info.size(); j++) {
+					errors += (info[j] ^ received[j]);
+				}
+				mean[i] += (double)errors/info.size();
 			}
 		}
 
 		std::cout << "p " << p << ": ";
-		for (int i = 0; i < encoders.size(); i++) {
-			std::vector<int> received = encoders[i].getSequence();
-			int errors = 0;
-			for (int j = 0; j < info.size(); j++) {
-				errors += (info[j] ^ received[j]);
-			}
-			std::cout << (double)errors/info.size() << " ";
-			encoderP[i].push_back((double)errors/info.size());
+		for (int i = 0; i < 3; i++) {
+			std::cout << mean[i]/5 << " ";
+			encoderP[i].push_back(mean[i]/5);
 		}
 		std::cout << std::endl;
 	}
